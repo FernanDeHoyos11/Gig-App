162e4d1120884416abf0e21d3245156fbd34fe4c
FernanDeHoyos11 <fernandehoyos93@gmail.com>
2023-06-11 11:56:03 -0500

    app-gif-share-download

--- src/helpers/handleShare.js
+++ src/helpers/handleShare.js
@@ -4,7 +4,7 @@ const gifUrl = url; // Reemplaza con la URL del GIF
 const shareText = '¡Mira este GIF!';
 const encodedShareText = encodeURIComponent(shareText);
 const encodedGifUrl = encodeURIComponent(gifUrl);
-const whatsappLink = `https://web.whatsapp.com/send?text=${encodedShareText}%0A%0A${encodedGifUrl}`;
+const whatsappLink = `https://wa.me/?text=${encodedShareText}%0A%0A${encodedGifUrl}`;
 
 window.open(whatsappLink, '_blank');
 };
